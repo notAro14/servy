33e93f44a5dd17f502661ce634f5a3ec760ce39c
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2023-08-05 18:00:10 +0200

    feat: advanced pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,7 +2,9 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
+    |> emojify
     |> format_response
   end
 
@@ -13,28 +15,94 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, resp_body: "", status: nil}
   end
 
-  def route(conv) do
-    conv = %{conv | resp_body: "Bears, Lions, Tigers"}
+  def log(conv), do: IO.inspect(conv)
+
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | resp_body: "Bears, Lions, Tigers", status: 200}
+  end
+
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{conv | resp_body: "Teddy, Smokey, Paddington", status: 200}
+  end
+
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | resp_body: "Bear #{id}", status: 200}
+  end
+
+  def route(%{method: "GET", path: "/bears?" <> id} = conv) do
+    %{conv | resp_body: "Bear #{id}", status: 200}
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{conv | status: 403, resp_body: "Bears must never be deleted!"}
+  end
+
+  def route(%{method: _method, path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
+
+  def emojify(%{status: 200} = conv) do
+    emojies = String.duplicate("🎉", 5)
+    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
+
+    %{conv | resp_body: body}
+  end
+
+  def emojify(conv), do: conv
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
 
-request = """
-GET /wildthings HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
+# request = """
+# GET /wildthings HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
 
-"""
+# """
+
+# request = """
+# GET /bears HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
+
+# """
+
+# request = """
+# GET /bears/14 HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
+
+# """
+
+# request = """
+# GET /bigfoots HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
+
+# """
